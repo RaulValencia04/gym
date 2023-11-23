c791586c16f7a0061eeba1ecef2fc0dea6bdab2b
RaulValencia04 <raulvalenciau@gmail.com>
2023-11-23 02:17:52 -0600

    Reportes terminados2

--- src/main/java/Controller/LoadReportController.java
+++ src/main/java/Controller/LoadReportController.java
@@ -8,6 +8,7 @@ import Conexion.Conexion;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -52,7 +53,7 @@ public class LoadReportController extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoadReportController</title>");            
+            out.println("<title>Servlet LoadReportController</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet LoadReportController at " + request.getContextPath() + "</h1>");
@@ -70,56 +71,79 @@ public class LoadReportController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-@Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-    try {
-        Connection con = null;
-        Conexion conexionDB = new Conexion();
-        con = conexionDB.obtenerConexion();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
 
-        HttpSession session = request.getSession();
-        String correo = (String) session.getAttribute("correo");
-        int idUsuario = obtenerIdUsuarioPorCorreo(correo);
+            ClassLoader classLoader = LoadReportController.class.getClassLoader();
 
-        // Definir el mapa de parámetros
-        Map<String, Object> parametros = new HashMap<>();
-        parametros.put("usuario", idUsuario); // Valor del parámetro
+// Convierte el nombre del paquete en una ruta
+            String rutaRelativa = "com/example/";
 
-        // Ruta del archivo JasperReport (.jasper)
-        String rutaReporte = "/Users/raulvalencia/NetBeansProjects/gym/src/main/webapp/Reportes/report1.jasper";
+// Obtén la URL del recurso (debe estar en el directorio WEB-INF/classes)
+            URL url = classLoader.getResource(rutaRelativa);
 
-        // Llenar el informe con datos y parámetros
-        JasperPrint jasperPrint = JasperFillManager.fillReport(rutaReporte, parametros, con);
+// La URL puede ser nula si no se encuentra el recurso
+            if (url != null) {
+                // Obtén la ruta del archivo desde la URL
+                String rutaAbsoluta = url.getPath();
 
-        // Exportar el informe a formato PDF
-        byte[] pdfBytes = JasperExportManager.exportReportToPdf(jasperPrint);
+                // Imprime la ruta relativa
+                System.out.println("Ruta Relativa: " + rutaRelativa);
 
-        // Establecer la respuesta HTTP
-        response.setContentType("application/pdf");
-        response.setHeader("Content-Disposition", "inline; filename=informe.pdf");
-
-        // Enviar el archivo PDF al cliente
-        try (OutputStream outStream = response.getOutputStream()) {
-            outStream.write(pdfBytes);
-            outStream.flush();
-        }
-
-        // Cerrar la conexión a la base de datos
-        if (con != null) {
-            try {
-                con.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+                // Imprime la ruta absoluta
+                System.out.println("Ruta Absoluta: " + rutaAbsoluta);
+            } else {
+                System.out.println("El recurso no se encontró.");
             }
+
+            Connection con = null;
+            Conexion conexionDB = new Conexion();
+            con = conexionDB.obtenerConexion();
+
+            HttpSession session = request.getSession();
+            String correo = (String) session.getAttribute("correo");
+            int idUsuario = obtenerIdUsuarioPorCorreo(correo);
+
+            // Definir el mapa de parámetros
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("usuario", idUsuario); // Valor del parámetro
+
+            // Ruta del archivo JasperReport (.jasper)
+//        String rutaReporte = "/Users/raulvalencia/NetBeansProjects/gym/src/main/webapp/Reportes/report1.jasper";
+            String rutaReporte = "/Users/raulvalencia/NetBeansProjects/gym/src/main/webapp//report1.jasper";
+
+            // Llenar el informe con datos y parámetros
+            JasperPrint jasperPrint = JasperFillManager.fillReport(rutaReporte, parametros, con);
+
+            // Exportar el informe a formato PDF
+            byte[] pdfBytes = JasperExportManager.exportReportToPdf(jasperPrint);
+
+            // Establecer la respuesta HTTP
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "inline; filename=informe.pdf");
+
+            // Enviar el archivo PDF al cliente
+            try ( OutputStream outStream = response.getOutputStream()) {
+                outStream.write(pdfBytes);
+                outStream.flush();
+            }
+
+            // Cerrar la conexión a la base de datos
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            processRequest(request, response);
+        } catch (SQLException | JRException ex) {
+            Logger.getLogger(LoadReportController.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        processRequest(request, response);
-    } catch (SQLException | JRException ex) {
-        Logger.getLogger(LoadReportController.class.getName()).log(Level.SEVERE, null, ex);
     }
-}
-
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -132,11 +156,10 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response)
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-             
 
-      
     }
-        private int obtenerIdUsuarioPorCorreo(String correo) throws SQLException {
+
+    private int obtenerIdUsuarioPorCorreo(String correo) throws SQLException {
         int idUsuario = -1;
         Connection con = null;
         PreparedStatement statement = null;
